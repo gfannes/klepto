d5d85e3e2ab83a5b283252002b309f0c8d8ca8a4
Thomas Fannes <thomas.fannes@auro-3d.com>
2026-08-01 11:05:43 +0200

    Using correct pin for tilt, cleanup of ledc

--- esp32/rover/Rover.cpp
+++ esp32/rover/Rover.cpp
@@ -1,18 +1,9 @@
 #include <Rover.hpp>
 
-#define use_ledc 1
-
 void Rover::setup() {
   Serial.println("Rover.setup()");
 
   pinMode(flywheel_pin, OUTPUT);
-
-#if use_ledc
-  ESP32PWM::allocateTimer(0);
-// ESP32PWM::allocateTimer(1);
-// ESP32PWM::allocateTimer(2);
-// ESP32PWM::allocateTimer(3);
-#endif
 
   // Typical hobby-servo pulse range. Adjust if required by your servos.
   servo_tilt.setPeriodHertz(50);
@@ -23,7 +14,6 @@ void Rover::setup() {
   servo_tilt.write(0);
   servo_shoot.write(0);
 
-#if use_ledc
   static const int pwm_freq = 20000; // 20kHz, above audible range
   static const int pwm_res = 8;      // 0-255 duty
 
@@ -31,13 +21,7 @@ void Rover::setup() {
   const bool ok2 = ledcAttach(motor_a_in2, pwm_freq, pwm_res);
   const bool ok3 = ledcAttach(motor_b_in1, pwm_freq, pwm_res);
   const bool ok4 = ledcAttach(motor_b_in2, pwm_freq, pwm_res);
-  Serial.printf("Motor ledcAttach: %d %d %d %d\n", ok1, ok2, ok3, ok4);
-#else
-  pinMode(motor_a_in1, OUTPUT);
-  pinMode(motor_a_in2, OUTPUT);
-  pinMode(motor_b_in1, OUTPUT);
-  pinMode(motor_b_in2, OUTPUT);
-#endif
+  Serial.printf("Motor ledcAttach: %d %d %d %d\n", ok1, ok2, ok3, ok4);  
 
   stop();
 }
@@ -47,7 +31,7 @@ void Rover::shoot() {
   delay(1000);
   servo_shoot.attach(servo_shoot_pin, 500, 2400);
   servo_shoot.write(70);
-  servo_tilt.attach(servo_shoot_pin, 500, 2400);
+  servo_tilt.attach(servo_tilt_pin, 500, 2400);
   servo_tilt.write(20);
   delay(500);
   servo_shoot.write(0);
@@ -64,7 +48,6 @@ void Rover::drive(int l, int r) {
 }
 
 void Rover::drive_(int in1, int in2, int speed) {
-#if use_ledc
   speed = constrain(speed, -255, 255);
   if (speed >= 0) {
     ledcWrite(in1, 0);
@@ -73,29 +56,13 @@ void Rover::drive_(int in1, int in2, int speed) {
     ledcWrite(in1, -speed);
     ledcWrite(in2, 0);
   }
-#else
-  if (speed >= 0) {
-    analogWrite(in1, 0);
-    analogWrite(in2, speed);
-  } else {
-    analogWrite(in1, -speed);
-    analogWrite(in2, 0);
-  }
-#endif
 }
 
 void Rover::stop() {
   digitalWrite(flywheel_pin, LOW);
 
-#if use_ledc
   ledcWrite(motor_a_in1, 0);
   ledcWrite(motor_a_in2, 0);
   ledcWrite(motor_b_in1, 0);
   ledcWrite(motor_b_in2, 0);
-#else
-  analogWrite(motor_a_in1, 0);
-  analogWrite(motor_a_in2, 0);
-  analogWrite(motor_b_in1, 0);
-  analogWrite(motor_b_in2, 0);
-#endif
 }
