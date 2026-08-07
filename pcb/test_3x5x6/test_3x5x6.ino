d93cba3a72611ba443fbc9520ea4531e8e920761
Geert Fannes <geert.fannes@gmail.com>
2026-08-07 23:31:33 +0200

    Test for spinners + servos

--- pcb/test_3x5x6/test_3x5x6.ino
+++ pcb/test_3x5x6/test_3x5x6.ino
@@ -5,8 +5,8 @@ constexpr uint8_t DC130_PIN = 33;
 
 constexpr uint8_t T1_A_IN1 = 1;
 constexpr uint8_t T1_A_IN2 = 3;
- // constexpr uint8_t T1_A_IN1 = 22;
- // constexpr uint8_t T1_A_IN2 = 23;
+// constexpr uint8_t T1_A_IN1 = 22;
+// constexpr uint8_t T1_A_IN2 = 23;
 constexpr uint8_t T1_B_IN1 = 19;
 constexpr uint8_t T1_B_IN2 = 18;
 
@@ -21,42 +21,52 @@ Servo servo2;
 
 void stopAllMotors()
 {
+
     digitalWrite(DC130_PIN, LOW);
 
-    digitalWrite(T1_A_IN1, LOW);
-    digitalWrite(T1_A_IN2, LOW);
-    digitalWrite(T1_B_IN1, LOW);
-    digitalWrite(T1_B_IN2, LOW);
+    servo1.write(0);
+    servo2.write(0);
+
+    ledcWrite(T1_A_IN1, 0);
+    ledcWrite(T1_A_IN2, 0);
+    ledcWrite(T1_B_IN1, 0);
+    ledcWrite(T1_B_IN2, 0);
 }
 
-void testDC130Motor()
+void testDC130Motor(bool on)
 {
-    Serial.println("DC130 motor: ON");
-    // analogWrite(DC130_PIN, 255);
-    digitalWrite(DC130_PIN, HIGH);
-    delay(MOTOR_TEST_MS);
-    // analogWrite(DC130_PIN, 0);
-    digitalWrite(DC130_PIN, LOW);
-    Serial.println("DC130 motor: OFF");
-    delay(PAUSE_MS);
+    if (on)
+    {
+        Serial.println("DC130 motor: ON");
+        // analogWrite(DC130_PIN, 255);
+        digitalWrite(DC130_PIN, HIGH);
+        delay(MOTOR_TEST_MS);
+    }
+    else
+    {
+        // analogWrite(DC130_PIN, 0);
+        digitalWrite(DC130_PIN, LOW);
+        Serial.println("DC130 motor: OFF");
+        delay(PAUSE_MS);
+    }
 }
 
 void driveT1(uint8_t in1, uint8_t in2, int direction)
 {
     if (direction > 0)
     {
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
+        ledcWrite(in1, 255);
+        ledcWrite(in2, 0);
     }
     else if (direction < 0)
     {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
+        ledcWrite(in1, 0);
+        ledcWrite(in2, 255);
     }
     else
     {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW);
+        ledcWrite(in1, 0);
+        ledcWrite(in2, 0);
     }
 }
 
@@ -77,22 +87,26 @@ void testT1Motor(const char *name, uint8_t in1, uint8_t in2)
     delay(PAUSE_MS);
 }
 
-void testServo(Servo &servo, const char *name)
+void testServos(Servo &servo1, Servo &servo2)
 {
-    Serial.printf("%s: 45 degrees\n", name);
-    servo.write(45);
+    Serial.printf("45 degrees\n");
+    servo1.write(45);
+    servo2.write(45);
     delay(1000);
 
-    Serial.printf("%s: 90 degrees\n", name);
-    servo.write(90);
+    Serial.printf("90 degrees\n");
+    servo1.write(90);
+    servo2.write(90);
     delay(1000);
 
-    Serial.printf("%s: 135 degrees\n", name);
-    servo.write(135);
+    Serial.printf("135 degrees\n");
+    servo1.write(135);
+    servo2.write(135);
     delay(1000);
 
-    Serial.printf("%s: return to 90 degrees\n", name);
-    servo.write(90);
+    Serial.printf("return to 90 degrees\n");
+    servo1.write(90);
+    servo2.write(90);
     delay(PAUSE_MS);
 }
 
@@ -101,19 +115,23 @@ void setup()
     Serial.begin(115200);
 
     pinMode(DC130_PIN, OUTPUT);
-    pinMode(T1_A_IN1, OUTPUT);
-    pinMode(T1_A_IN2, OUTPUT);
-    pinMode(T1_B_IN1, OUTPUT);
-    pinMode(T1_B_IN2, OUTPUT);
-    stopAllMotors();
 
     // Typical hobby-servo pulse range. Adjust if required by your servos.
     servo1.setPeriodHertz(50);
     servo2.setPeriodHertz(50);
     servo1.attach(SERVO_1_PIN, 500, 2400);
     servo2.attach(SERVO_2_PIN, 500, 2400);
-    servo1.write(90);
-    servo2.write(90);
+    servo1.write(0);
+    servo2.write(0);
+
+    static const int pwm_freq = 20000; // 20kHz, above audible range
+    static const int pwm_res = 8; // 0-255 duty
+
+    const bool ok1 = ledcAttach(T1_A_IN1, pwm_freq, pwm_res);
+    const bool ok2 = ledcAttach(T1_A_IN2, pwm_freq, pwm_res);
+    const bool ok3 = ledcAttach(T1_B_IN1, pwm_freq, pwm_res);
+    const bool ok4 = ledcAttach(T1_B_IN2, pwm_freq, pwm_res);
+    stopAllMotors();
 
     Serial.println("\nESP32 device test starts in 3 seconds...");
     delay(3000);
@@ -123,13 +141,14 @@ void loop()
 {
     Serial.println("\n--- Starting test cycle ---");
 
-    testDC130Motor();
+    testDC130Motor(true);
+
+    testServos(servo1, servo2);
+
+    testDC130Motor(false);
 
     testT1Motor("T1 motor A", T1_A_IN1, T1_A_IN2);
     testT1Motor("T1 motor B", T1_B_IN1, T1_B_IN2);
-
-    testServo(servo1, "Servo 1");
-    testServo(servo2, "Servo 2");
 
     stopAllMotors();
     Serial.println("--- Test cycle complete; repeating in 2 seconds ---");
