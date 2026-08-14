d639949fa598c4aef4e0137eb47be29b998ae011
Geert Fannes <geert.fannes@gmail.com>
2026-08-14 12:37:23 +0200

    Fixed install when folder also exist

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,19 @@
+def mp_mkdir(path)
+    sh "mpremote fs ls #{path}", verbose: false do |ok, _res|
+        return if ok
+    end
+    sh "mpremote fs mkdir #{path}"
+end
+
+task :install do
+    sh 'mpremote exec "from mpos import AppManager; AppManager.restart_launcher()"'
+    sleep 0.2
+    mp_mkdir ':/apps'
+    mp_mkdir ':/apps/com.fannes.klepto'
+    sh 'mpremote fs cp mp/main.py :/apps/com.fannes.klepto/main.py'
+    sh 'mpremote fs cp mp/MANIFEST.JSON :/apps/com.fannes.klepto/MANIFEST.JSON'
+    sh 'mpremote fs cp mp/icon_64x64.png :/apps/com.fannes.klepto/icon_64x64.png'
+end
 task :upload do
     sh 'mpremote exec "from mpos import AppManager; AppManager.restart_launcher()"'
     sleep 0.2
